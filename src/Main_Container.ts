7c8663e25e8cd91ac3e63df6338d3880481edce6
DmitriyNarzyaev <nozaki@yandex.ru>
2020-08-21 15:50:40 +0300

    initial dial

--- src/Main_Container.ts
+++ src/Main_Container.ts
@@ -5,19 +5,21 @@ export default class Main_Container extends Container {
 	public static readonly WIDTH:number = 3000;
 	public static readonly HEIGHT:number = 1500;
 	private _background:Sprite;
+	private _dial:Sprite;
 
 	constructor() {
 		super();
-
 		this.initImages();
 	}
 
 	private initImages():void {
         const loader:Loader = new Loader();
 		loader.add("background", "background.jpg");
+		loader.add("dial", "dial.png");
 		
 		loader.on("complete", ()=> {
 			this.initBackground();
+			this.initAnalogClock();
 		});
 		loader.load();
 	}
@@ -26,5 +28,12 @@ export default class Main_Container extends Container {
 		this._background = Sprite.from("background");
 		this._background.tint = 0xaaaaaa;
 		this.addChild(this._background);
+	}
+
+	private initAnalogClock():void {
+		this._dial = Sprite.from("dial");
+		this.addChild(this._dial);
+		this._dial.x = Main_Container.WIDTH/2 - this._dial.width/2;
+		this._dial.y = Main_Container.HEIGHT/2 - this._dial.height/2;
 	}
 }
