b0be664c21e3d4dc728d6d27f2d0131f485be2f2
DmitriyNarzyaev <nozaki@yandex.ru>
2020-08-21 15:42:38 +0300

    initial background

--- src/Main_Container.ts
+++ src/Main_Container.ts
@@ -1,20 +1,30 @@
 import Container = PIXI.Container;
-import { Graphics } from "pixi.js";
+import { Loader, Sprite } from "pixi.js";
 
 export default class Main_Container extends Container {
 	public static readonly WIDTH:number = 3000;
 	public static readonly HEIGHT:number = 1500;
+	private _background:Sprite;
 
 	constructor() {
 		super();
 
-		this.background();
+		this.initImages();
 	}
 
-	private background():void {
-		let background: Graphics = new Graphics;
-		background.beginFill(0x00ff48);
-		background.drawRect(0, 0, Main_Container.WIDTH, Main_Container.HEIGHT);
-		this.addChild(background);
+	private initImages():void {
+        const loader:Loader = new Loader();
+		loader.add("background", "background.jpg");
+		
+		loader.on("complete", ()=> {
+			this.initBackground();
+		});
+		loader.load();
+	}
+
+	private initBackground():void {
+		this._background = Sprite.from("background");
+		this._background.tint = 0xaaaaaa;
+		this.addChild(this._background);
 	}
 }
